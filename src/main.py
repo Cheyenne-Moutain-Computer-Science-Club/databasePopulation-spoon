c7135a155e689a1103b6b1558e5c9e666cd615ef
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2023-01-10 08:17:19 -0700

    Async fix

--- src/main.py
+++ src/main.py
@@ -1,43 +1,67 @@
+import asyncio
 import csv
 import random
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Firestore setup
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+# cred = credentials.ApplicationDefault()
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
 
-
-# multConst = 1987654787.8379
 
 async def makeFsDoc(dict, uuid):
     doc_ref = db.collection("users").document(uuid)
     await doc_ref.set(dict)
 
-with open("input.csv", "r") as f:
-    raw = csv.reader(f)
-    keys = next(raw)
-    data = [dict(zip(keys, values)) for values in raw]
+outList = []
 
-    idList = []
-    for x in data:
-        # Generate unique UUID
-        uuid = str(random.randrange(10000000, 99999999))
-        while True:
-            if idList.count(uuid) > 0:
-                uuid = str(random.randrange(10000000, 99999999))
-            else:
-                break
 
-        idList.append(uuid)
-        # Add a new key for ID
-        x.update(id=uuid)
+async def parser():
+    # Parse CSV into docs
+    with open("input.csv", "r") as f:
+        raw = csv.reader(f)
+        keys = next(raw)
+        data = [dict(zip(keys, values)) for values in raw]
 
-        # Add various keys w/ default values for use elsewhere
-        x.update(score=0)
-        x.update(outBy=0)
-        x.update(tagged=[])
+        idList = []
+        for x in data:
+            # Generate unique UUID
+            uuid = str(random.randrange(10000000, 99999999))
+            while True:
+                if idList.count(uuid) > 0:
+                    uuid = str(random.randrange(10000000, 99999999))
+                else:
+                    break
 
-        # Add to Firestore
-        makeFsDoc(x, uuid)
+            idList.append(uuid)
+            # Add a new key for ID
+            x.update(id=uuid)
+
+            # Add various keys w/ default values for use elsewhere
+            x.update(score=0)
+            x.update(outBy=0)
+            x.update(tagged=[])
+
+            # Add to output list
+            ref = {
+                "name": x["name"],
+                "id": uuid
+            }
+            outList.append(ref)
+
+            # Add to Firestore
+            await makeFsDoc(x, uuid)
+
+
+async def outputWriter():
+    # Write to output CSV
+    cols = ["name", "id"]
+    with open("out.csv", "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=cols)
+        writer.writeheader
+        for data in outList:
+            writer.writerow(data)
+
+asyncio.run(parser())
+asyncio.run(outputWriter())
